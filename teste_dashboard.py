26e1cb6c1de2978d76c6c61244100fc0fd5df81a
Fabio <fabiohitoshisakaguchi@gmail.com>
2024-09-10 13:08:52 -0300

    Com base no arquivo do branch quest_um, foi adicionado o trecho para abertura dos arquivo de chuva e nivel de rio, plotagem dos gráficos e analise de Mann-Kendall

--- teste_dashboard.py
+++ teste_dashboard.py
@@ -19,6 +19,9 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from plotly.colors import qualitative
 import pandas as pd
+#import pyhomogeneity as hg
+import pymannkendall as mk
+
 #from dash import Dash, html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -55,7 +58,7 @@ selector = html.Div([
         multiple=True)
         ])
 
-# Layout do aplicativo
+# Trecho para questionario 1-------------------------------------------------------
 quest_um = html.Div([
     html.H3("Questionário sobre Eventos",style={'margin-top':'1cm'}),
     
@@ -120,6 +123,9 @@ def class_evento(n_clicks, resposta_a, resposta_b, resposta_c):
         elif resposta_a == 'Sim' and resposta_b == 'Não' and resposta_c == 'Não':
             evento = 'ENCHENTE'
             descricao = 'É definida pela elevação do nível d’água no canal de drenagem devido ao aumento da vazão, atingindo a cota máxima do canal de drenagem, porém, sem extravasar (sem o rio transbordar).'
+        elif resposta_a == 'Sim' and resposta_b == 'Não' and resposta_c == 'Sim':
+            evento = 'ENXURRADA'
+            descricao = 'É um escoamento superficial concentrado e com alta energia de transporte, que pode ou não estar associado aos rios. As enxurradas são capazes de carregar pessoas, carros, caçambas e outros objetos. Geralmente ocorrem em áreas com deficiência no sistema de drenagem, em locais com maior declividade, entre outras.'
         elif resposta_a == 'Sim' and resposta_b == 'Sim' and resposta_c == 'Não':
             evento = 'INUNDAÇÃO'
             descricao = 'Representa o transbordamento de um curso d’água, atingindo a planície de inundação ou área de várzea.'
@@ -132,13 +138,117 @@ def class_evento(n_clicks, resposta_a, resposta_b, resposta_c):
         
         return html.Div([
             html.H3(f"Evento Identificado: {evento}"),
-            html.P(descricao)
+            html.P(descricao),
+            html.P('Fonte: Glossário Transdiciplinar (Projeto COPE - CEMADEN)')
         ], style={'backgroundColor': '#f0f0f0', 'padding': '20px', 'border-radius': '5px', 'margin-top': '1cm'})
     return ""
 
+#Trecho para analise de serie de chuva --------------------------------------------------------------
+arquivo_chuva = "dadosreais.csv"  # Alterar para colocar o caminho do arquivo de chuva anexado
+dados_chuva = pd.read_csv(arquivo_chuva, sep=';', usecols=[0, 1], header=None)
+dados_chuva.columns = ['Data', 'Precipitacao']
+dados_chuva['Data'] = pd.to_datetime(dados_chuva['Data'], format='%Y-%m-%d') #formato que vem do site do INMET
+dados_chuva = dados_chuva[dados_chuva['Precipitacao'].notna()]
 
-app.layout = html.Div([titulo, selector,quest_um])
+resultado_mk_chuva = mk.original_test(dados_chuva['Precipitacao']) #teste de Mann-Kendall
 
+analise_chuva = html.Div([
+    html.H3("Análise da Série Temporal de Precipitação", style={'top-margin':'1cm'}),
+    html.Div(id='resultado_mk_chuva', style={'padding': '20px', 'backgroundColor': '#f0f0f0', 'top-margin':'1cm'}),
+    dcc.Graph(id='grafico_precipitacao'),
+    ])
+
+@app.callback(
+    [Output('resultado_mk_chuva', 'children'),
+     Output('grafico_precipitacao', 'figure'),
+     Input('grafico_precipitacao', 'id')]
+)
+def tendencia_chuva(_):
+    # Análise do resultado do teste de Mann-Kendall
+    if resultado_mk_chuva.trend == 'no trend':
+        desc_tend_chuva = 'De acordo com o teste de Mann-Kendall, não foi detectada tendência de mudança nas chuvas.'
+    elif resultado_mk_chuva.trend == 'increasing':
+        desc_tend_chuva = 'Foi detectada uma tendência de aumento nas chuvas.'
+    else:
+        desc_tend_chuva = 'Foi detectada uma tendência de diminuição nas chuvas.'
+
+    # Criar gráfico de precipitação
+    grafico_chuva = {
+        'data': [
+            go.Bar(
+                x=dados_chuva['Data'],
+                y=dados_chuva['Precipitacao'],
+                name='Precipitação'
+            )
+        ],
+        'layout': go.Layout(
+            title='Série Temporal de Precipitação',
+            xaxis_title='Data',
+            yaxis_title='Precipitação (mm)',
+            xaxis=dict(
+                tickformat='%d-%m-%Y',
+                dtick='M12',  # Marca de 1 ano
+            ),
+            shapes=[],
+            hovermode='x'
+        )
+    }
+    return desc_tend_chuva, grafico_chuva
+   
+#trecho para analise de nivel do rio --------------------------------------------------------------
+arquivo_rio = "rio.csv"  # Alterar para colocar o caminho do arquivo de rio anexado
+dados_rio = pd.read_csv(arquivo_rio, sep=';', usecols=[0, 1], header=None)
+dados_rio.columns = ['Data', 'Nivel']
+dados_rio['Data'] = pd.to_datetime(dados_rio['Data'], format='%Y-%m-%d') #diferente do site da ANA, mas mudado para ficar no mesmo formato da chuva
+dados_rio = dados_rio[dados_rio['Nivel'].notna()]
+
+resultado_mk_rio = mk.original_test(dados_rio['Nivel']) #teste de Mann-Kendall
+
+analise_rio = html.Div([
+    html.H3("Análise da Série Temporal de Nível do Rio", style={'top-margin':'1cm'}),
+    html.Div(id='resultado_mk_rio', style={'padding': '20px', 'backgroundColor': '#f0f0f0', 'top-margin':'1cm'}),
+    dcc.Graph(id='grafico_rio'),
+    ])
+
+@app.callback(
+    [Output('resultado_mk_rio', 'children'),
+     Output('grafico_rio', 'figure'),
+     Input('grafico_rio', 'id')]
+)
+def tendencia_rio(_):
+    # Análise do resultado do teste de Mann-Kendall
+    if resultado_mk_rio.trend == 'no trend':
+        desc_tend_rio = 'De acordo com o teste de Mann-Kendall, não foi detectada tendência de mudança no nível do rio.'
+    elif resultado_mk_rio.trend == 'increasing':
+        desc_tend_rio = 'Foi detectada uma tendência de aumento no nível do rio.'
+    else:
+        desc_tend_rio = 'Foi detectada uma tendência de diminuição no nível do rio.'
+
+    # Criar gráfico de nível do rio
+    grafico_rio = {
+        'data': [
+            go.Scatter(
+                x=dados_rio['Data'],
+                y=dados_rio['Nivel'],
+                mode='lines',
+                name='Nível_do_Rio'
+            )
+        ],
+        'layout': go.Layout(
+            title='Série Temporal de Nível do Rio',
+            xaxis_title='Data',
+            yaxis_title='Nível (cm)',
+            xaxis=dict(
+                tickformat='%d-%m-%Y',
+                dtick='M1',  # Marca de 1 mês, apenas para este set de dados, porque tem menos de um ano, se set maior, o que é necessário, alterar para M12
+            ),
+            hovermode='x'
+        )
+    }
+
+    return desc_tend_rio, grafico_rio
+
+app.layout = html.Div([titulo, selector,quest_um, analise_chuva, analise_rio])
 
 app.callback( Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
